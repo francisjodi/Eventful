f65c710fd221c3dec1c3fff34784a31ccf3a0092
hjohns28 <hjohns28@lion.lmu.edu>
2019-08-12 11:59:48 -0700

    added to models

--- event_models.py
+++ event_models.py
@@ -3,39 +3,12 @@ from google.appengine.ext import ndb
 class Event(ndb.Model):
     event_name =  ndb.StringProperty(required=True)
     location =  ndb.StringProperty(required=True)
-    college_name =  ndb.StringProperty(required=True)
     organization_name =  ndb.StringProperty(required=True)
-    category =  ndb.StringProperty(required=True)
-    date =  ndb.StringProperty(required=True)
-    time =  ndb.StringProperty(required=True)
-    image =  ndb.StringProperty(required=True)
+    date_and_time =  ndb.DateTimeProperty(required=True)
     additional_info =  ndb.StringProperty(required=True)
+    category =  ndb.StringProperty(required=True, choices=["All", "Academic", "Athletics", "Arts", "Service", "Other"])
+    college_name =  ndb.Property(required=True, choices=["Loyola Marymount University", "University of California Los Angeles", "University of Southern California", "Pepperdine University"])
 
-#One to One
-class Wand(ndb.Model):
-    length = ndb.FloatProperty(required=True)
-    material = ndb.StringProperty(required=True)
-    core = ndb.StringProperty(required=True)
-    owner = ndb.KeyProperty(Student)
-
-#One to Many
-class House(ndb.Model):
-      name = ndb.StringProperty(required=True)
-      mascot = ndb.StringProperty(required=False)
-      students = ndb.KeyProperty(Student, repeated=True)
-
-# Many to Many
-class Course(ndb.Model):
-      name = ndb.StringProperty()
-      location = ndb.StringProperty()
-
-class Enrollment(ndb.Model):
-      student = ndb.KeyProperty(Student)
-      course = ndb.KeyProperty(Course)
-
-#One to Many
-#Generally, teachers teach more than one class
-#While this hasn't happened yet at Hogwarts, it's feasible, so we'll make repeated = True
 
 class Teacher(ndb.Model):
     name = ndb.StringProperty(required=True)
