31ace91093b25b920119645fe0230f03d7e9fe12
hjohns28 <hjohns28@lion.lmu.edu>
2019-08-13 12:02:09 -0700

    time changes

--- event_models.py
+++ event_models.py
@@ -8,6 +8,7 @@ class Event(ndb.Model):
     location =  ndb.StringProperty(required=True)
     organization_name =  ndb.StringProperty(required=True)
     date_and_time =  ndb.DateProperty(required=True)
+    time = ndb.TimeProperty(required=True)
     additional_info =  ndb.StringProperty(required=False)
     category =  ndb.StringProperty(required=True)
     college_name =  ndb.StringProperty(required=True)
