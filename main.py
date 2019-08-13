17c773790fb9bd344e029fddbfa75d4a89f5915e
warrendlee1 <warrendlee1@gmail.com>
2019-08-13 11:38:42 -0700

    calendar

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from google.appengine.api import users
 from event_models import Event
 from seed_events import seed_data
 import datetime
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -65,6 +70,40 @@ class CalendarHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_env.get_template('/templates/calendar.html')
         self.response.write(template.render())
+
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                            maxResults=10, singleEvents=True,
+                                            orderBy='startTime').execute()
+        events = events_result.get('items', [])
+    
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
         
 
 class SearchHandler(webapp2.RequestHandler):
@@ -105,6 +144,8 @@ class AddEventHandler(webapp2.RequestHandler):
 
         event_block = Event.query().order(Event.category).fetch()
         self.response.write(template.render({'event_block' : event_block}))
+
+
 
 
 class LoadDataHandler(webapp2.RequestHandler):
